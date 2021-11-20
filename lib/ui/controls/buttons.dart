59f5bd52eddcf79aaa235f7f39ec1b420b4c691c
Eleandro Duzentos <eleandro@inbox.ru>
2021-11-20 22:04:51 +0100

    feat: added ShadowedButton

--- lib/ui/controls/buttons.dart
+++ lib/ui/controls/buttons.dart
@@ -1,3 +1,45 @@
+
+class ShadowedButton extends StatelessWidget {
+  final Widget child;
+  final VoidCallback onPress;
+  final Color? backgroundColor;
+
+  const ShadowedButton({
+    Key? key,
+    required this.child,
+    required this.onPress,
+    this.backgroundColor,
+  }) : super(key: key);
+
+  const ShadowedButton.icon({
+    Key? key,
+    required this.child,
+    required this.onPress,
+    this.backgroundColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor ?? Colors.white,
+        borderRadius: BorderRadius.circular(1000),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(.1),
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: BaseButton(
+        backgroundColor: backgroundColor,
+        child: child,
+        onPress: onPress,
+      ),
+    );
+  }
+}
+
 class BaseButton extends StatelessWidget {
   final Widget child;
   final VoidCallback onPress;
