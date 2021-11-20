55690732f2ac15e35cbfc9f6489f4072e2ce38a0
Eleandro Duzentos <eleandro@inbox.ru>
2021-11-20 22:06:02 +0100

    feat: added TurnOnOffButton

--- lib/ui/controls/buttons.dart
+++ lib/ui/controls/buttons.dart
@@ -1,3 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+class TurnOnOffButton extends StatelessWidget {
+  final VoidCallback onPress;
+  const TurnOnOffButton({
+    Key? key,
+    required this.onPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShadowedIconButton(
+      backgroundColor: Colors.red.shade300,
+      onPress: onPress,
+      icon: Transform.translate(
+        offset: const Offset(0, -2),
+        child: const Icon(
+          FeatherIcons.power,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
 
 class ColoredButton extends StatelessWidget {
   final Color color;
