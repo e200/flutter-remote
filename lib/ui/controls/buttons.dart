55d04940260d4e10c714fef4d20d0ef377825c5b
Eleandro Duzentos <eleandro@inbox.ru>
2021-11-20 22:05:35 +0100

    feat: added ColoredButton

--- lib/ui/controls/buttons.dart
+++ lib/ui/controls/buttons.dart
@@ -1,4 +1,48 @@
 
+class ColoredButton extends StatelessWidget {
+  final Color color;
+  final VoidCallback onPress;
+
+  const ColoredButton({
+    Key? key,
+    required this.color,
+    required this.onPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseButton(
+      onPress: onPress,
+      child: SizedBox(
+        width: 40,
+        height: 40,
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Opacity(
+              opacity: .2,
+              child: Container(
+                width: 40,
+                height: 40,
+                color: color,
+                padding: const EdgeInsets.all(5),
+              ),
+            ),
+            ClipOval(
+              child: Container(
+                width: 30,
+                height: 30,
+                color: color,
+                padding: const EdgeInsets.all(5),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class ShadowedIconButton extends StatelessWidget {
   final Widget icon;
   final VoidCallback onPress;
