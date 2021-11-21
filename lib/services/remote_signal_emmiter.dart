db840d8f14ef19d3e25d1b8a2fe5c28ce590fed2
Eleandro Duzentos <eleandro@inbox.ru>
2021-11-21 17:11:38 +0100

    feat: now signal emissions run in isolates

--- lib/services/remote_signal_emmiter.dart
+++ lib/services/remote_signal_emmiter.dart
@@ -1,9 +1,30 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter_remote/remote_codes/lg.dart';
 import 'package:ir_sensor_plugin/ir_sensor_plugin.dart';
+import 'package:isolate_handler/isolate_handler.dart';
 import 'package:riverpod/riverpod.dart';
 
-final remoteSignalEmmiter =
-    Provider<RemoteSignalEmmiter>((ref) => LgRemoteSignalEmmiter());
+IsolateHandler _isolates = IsolateHandler();
+
+void _entryPoint(Map<String, dynamic> context) {
+  final _messenger = HandledIsolate.initialize(context);
+
+  _messenger.listen((pattern) {
+    _emmit(pattern);
+  });
+}
+
+void _emmit(String json) async {
+  final _map = jsonDecode(json);
+
+  await IrSensorPlugin.transmitString(pattern: _map['pattern']!);
+
+  _isolates.kill(_map['isolate']!);
+}
+
+final remoteSignalEmmiter = Provider<RemoteSignalEmmiter>((ref) => LgRemoteSignalEmmiter());
 
 abstract class RemoteSignalEmmiter {
   void turnOnOff();
@@ -113,7 +134,7 @@ class LgRemoteSignalEmmiter implements RemoteSignalEmmiter {
   }
 
   @override
-  void volumeUp() async {
+  void volumeUp() {
     emmit(LgRemoteCode.volumeUp);
   }
 
@@ -148,6 +169,20 @@ class LgRemoteSignalEmmiter implements RemoteSignalEmmiter {
   }
 
   void emmit(String pattern) {
-    IrSensorPlugin.transmitString(pattern: pattern);
+    final _isolateName = Random().nextInt(100).toString();
+
+    _isolates = IsolateHandler();
+
+    _isolates.spawn(
+      _entryPoint,
+      name: _isolateName,
+      onInitialized: () => _isolates.send(
+        jsonEncode({
+          'isolate': _isolateName,
+          'pattern': pattern,
+        }),
+        to: _isolateName,
+      ),
+    );
   }
 }
